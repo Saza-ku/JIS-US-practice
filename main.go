40e8ebf6c49c12013e99457d7aa4d46e11b1f4ba
Saza-ku <the.latticeheart@gmail.com>
2022-01-16 00:54:29 +0900

    標準入力と標準出力を受け持つ goroutine と残り時間と得点の管理をする goroutine を生やすところまで

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ch := make(chan int)
+	go func() {
+		r := bufio.NewReaderSize(os.Stdin, 1000000)
+		x := 0
+		for {
+			s, _, err := r.ReadLine()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(s))
+			x++
+			ch <- x
+		}
+	}()
+	x := 0
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			fmt.Println("DONE")
+			fmt.Println(x)
+			return
+		case x = <-ch:
+		}
+	}
 }
